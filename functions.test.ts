1bc4c4ed2ac1ff625c198f5c074882d8a26597af
Cody <relationalprogrammer@gmail.com>
2021-11-21 16:54:19 -0600

    tests for utils.js passed

--- functions.test.ts
+++ functions.test.ts
@@ -2,4 +2,27 @@ const {shuffleArray} = require('./utils')
 
 describe('shuffleArray should', () => {
     // CODE HERE
+    let sample = [1,2,3]
+    let value = shuffleArray(sample)
+
+    test('shuffleArray returns an array', () => {
+        // expect(typeof arrLength(sample1)).toBe("number");
+        expect(Array.isArray(value)).toBe(true)
+    })
+
+    test('shuffleArray returns an array with the same length as the original', () => {
+        expect(value.length).toEqual(shuffleArray(sample).length)
+    })
+
+    test('shuffleArray returns a copied array with unmodified elements', () => {
+        expect(value.includes(sample[0])).toBe(true)
+        expect(value.includes(sample[1])).toBe(true)
+        expect(value.includes(sample[2])).toBe(true)
+    })
+
+    test('shuffleArray shuffles the array', () => {
+        expect(value[0]).not.toEqual(sample[0])
+        expect(value[1]).not.toEqual(sample[1])
+        expect(value[2]).not.toEqual(sample[2])
+    })
 })
